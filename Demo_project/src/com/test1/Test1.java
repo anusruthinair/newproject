66cde4cd94fdcca44363af9d8e1801006a114d03
Anu <anusruthinair@gmail.com>
2022-02-17 22:39:15 +0530

    my 2nd project

---
+++ Demo_project/src/com/test1/Test1.java
@@ -0,0 +1,23 @@
+package com.test1;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class Test1 {
+	int x=10;
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("main start");
+		Test1 t=new Test1();
+		System.out.println(t.x);
+		t=null;
+		
+		System.gc();
+		Thread.sleep(2000);
+		System.out.println("main end");
+	}
+	protected void finalize()
+	{
+		System.out.println("object destroyed");
+	}
+}
---
+++ Demo_project/src/com/test1/Test2.java
@@ -0,0 +1,8 @@
+package com.test1;
+
+public class Test2 {
+public void m2()
+{
+	System.out.println("m2 running");
+}
+}
